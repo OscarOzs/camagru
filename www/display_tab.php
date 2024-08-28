4e91e0babc3827cf7ad14a69311ff9d9d5c6b839
Oscar Ozserttas <ozserttas.oscar@gmail.com>
2024-08-28 15:36:57 +0200

    Reset

--- www/display_tab.php
+++ www/display_tab.php
@@ -1,25 +1,17 @@
 <?php
 
-$host = getenv('DB_HOST');
-$username = getenv('DB_USERNAME');
-$password = getenv('DB_PASSWORD');
-$database = getenv('DB_DATABASE');
-
-$connect = mysqli_connect($host, $username, $password, $database);
-
-if (!$connect) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+$connect = mysqli_connect(
+    getenv('DB_HOST'),
+    getenv('DB_USERNAME'),
+    getenv('DB_PASSWORD'),
+    getenv('DB_DATABASE')
+);
 
 $table_name = "php_docker_table";
 
 $query = "SELECT * FROM $table_name";
 
 $response = mysqli_query($connect, $query);
-
-if (!$response) {
-    die("Query failed: " . mysqli_error($connect));
-}
 
 echo "<strong>$table_name: </strong>";
 while($i = mysqli_fetch_assoc($response))
@@ -29,5 +21,3 @@ while($i = mysqli_fetch_assoc($response))
     echo "<p>".$i['date_created']."</p>";
     echo "<hr>";
 }
-
-mysqli_close($connect);
